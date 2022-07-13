5f8a70c4941c2df92b583e61a73887af87972854
Kacper Żarna <kacper-zarna@wp.pl>
2022-07-13 18:35:22 +0200

    Added loggedIn state and routes to Category menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./components/HomePage/HomePage";
 import ProductPage from "./components/ProductPage/ProductPage";
 import LoginPage from "./components/Login/LoginPage/LoginPage";
 import RegisterPage from "./components/Login/RegisterPage/RegisterPage";
+import { auth } from "./helpers/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
 function App() {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  });
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
+        <Navbar loggedIn={loggedIn} />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route
--- src/components/CategoryMenu/CategoryMenu.tsx
+++ src/components/CategoryMenu/CategoryMenu.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Paper } from "@mui/material";
 import CategoryTile from "../CategoryTile/CategoryTile";
+import { Link } from "react-router-dom";
 const CategoryMenu: React.FC<{
   categories: string[];
 }> = ({ categories }) => {
   return (
     <Paper elevation={1} sx={{ my: "2rem" }}>
       {categories.map((el: string, i: number) => {
-        return <CategoryTile category={el} key={i} />;
+        let route: string = el;
+        if (el === "men's clothing") route = "/mans";
+        if (el === "women's clothing") route = "/womans";
+
+        return (
+          <Link to={route} key={i}>
+            <CategoryTile category={el} />
+          </Link>
+        );
       })}
     </Paper>
   );
