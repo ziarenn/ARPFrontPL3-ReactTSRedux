62d750f925fd238d984d99d608135b2c644305a5
Kacper Żarna <kacper-zarna@wp.pl>
2022-07-16 22:21:21 +0200

    Started implementing Redux Thunk Middleware and migrating all the API calls to action creators

--- src/redux/actions/productActions.ts
+++ src/redux/actions/productActions.ts
@@ -1,5 +1,8 @@
 import { ActionTypes } from "../constants/action-types";
 import { Product } from "../../helpers/interfaces";
+import axios from "axios";
+import fakeStoreApi from "../../apis/fakeStoreApi";
+import { Dispatch } from "redux";
 export const setProduct = (product: Product) => {
   return {
     type: ActionTypes.SET_PRODUCT,
@@ -10,6 +13,13 @@ export const setProduct = (product: Product) => {
 export const removeProduct = (product: Product) => {
   return {
     type: ActionTypes.REMOVE_PRODUCT,
-    payload: product
-  }
-}
+    payload: product,
+  };
+};
+
+export const fetchProducts = () => {
+  return async function(dispatch: Dispatch) {
+    const response = await fakeStoreApi.get("/products");
+    dispatch({ type: ActionTypes.FETCH_PRODUCTS, payload: response.data });
+  };
+};
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { reducers } from "./reducers/index";
-import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 const store = configureStore({
   reducer: reducers,
+  middleware: [thunk],
   devTools: process.env.NODE_ENV !== "production",
 });
 
